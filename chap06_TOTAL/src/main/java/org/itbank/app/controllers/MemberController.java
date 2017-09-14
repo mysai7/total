cb04b6b8ba667612b6260f1d5c46bad490cd8316
1103-6 <1103-6@1103-6-PC>
2017-09-14 13:30:56 +0900

    개인정보 2차 수정

--- chap06_TOTAL/src/main/java/org/itbank/app/controllers/MemberController.java
+++ chap06_TOTAL/src/main/java/org/itbank/app/controllers/MemberController.java
@@ -2,14 +2,18 @@ package org.itbank.app.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/my")
 public class MemberController {
 	@GetMapping({"/", "/index"})
-	public String infoHandle() {
+	public String infoGetHandle() {
 		return "t_expr";
 	}
-	
+	@PostMapping({"/", "/index"})
+	public String infoPostHandle() {
+		return "t_expr";
+	}
 }
